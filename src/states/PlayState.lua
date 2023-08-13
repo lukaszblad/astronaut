23e70b94d50adcff5da3d51c08e6450fae002b67
Łukasz Błąd <lukasz.blad.97@gmail.com>
2023-08-13 17:25:42 +0200

    work on multipe meteorites implementation

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -2,20 +2,27 @@ PlayState = Class{__includes = BaseState}
 
 function PlayState:init()
     self.astronaut = Astronaut()
-    self.meteorites = {[1] = Meteorite('meteorite5')}
+    self.meteorites = Meteorite('meteorite5')
 
     self.timer = 0
 end
 
 function PlayState:update(dt)
-    self.timer = self.timer + dt
-    if self.timer > 0.5 then
-        randomMeteorite = 'meteorite' .. math.random(5) .. 'px'
-        table.insert(self.meteorites, Meteorite(randomMeteorite))
+    -- self.timer = self.timer + dt
+    -- if self.timer > 0.5 then
+    --     randomMeteorite = 'meteorite' .. math.random(5) .. 'px'
+    --     table.insert(self.meteorites, Meteorite(randomMeteorite))
 
-        -- resetting timer
-        self.timer = 0
-    end 
+    --     -- resetting timer
+    --     self.timer = 0
+    -- end 
+
+    -- updating meteorites
+    -- for index, meteorite in pairs(self.meteorites) do
+    --     index:update(dt)
+    -- end
+
+    self.meteorite:update(dt)
 
     -- updating astronaut
     self.astronaut:update(dt)
@@ -26,9 +33,11 @@ end
 
 function PlayState:render()
     -- render meteorite
-    for index, meteorite in pairs(self.meteorites) do
-        meteorite:render()
-    end
+    -- for index, meteor in pairs(self.meteorites) do
+    --     index:render()
+    -- end
+
+    self.meteorite:render()
 
     -- rendering astronaut
     self.astronaut:render()
